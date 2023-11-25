d7c375272e16d004708e767696e7d61797e46646
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-11-25 15:00:19 +0600

    Add New File

--- First_Try.py
+++ First_Try.py
@@ -10,13 +10,21 @@ str1 = "this is my first python programe"
 print(str1.capitalize())
 print(str1.find("this"))
 print(str1.upper())
-print(str1.lower())'''
+print(str1.lower())
 
 items = ["Hi",2,'H']
 items[0] = "Hello"
 print(items)
 print(type(items))
-?
 
-
-# Test
+dictio = 100
+print(dictio)
+var = int(input("Enter Number : "))
+print("The number is : ",var)
+if(var>4):
+    print("\nVar is greater than 4")
+else:
+    print("\nVar is less than 4")'''
+    
+    
+# New
