877fa32a50b8097bd8e2497f5ee4823353102caf
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-11-25 13:37:13 +0600

    Test

---
+++ First_Try.py
@@ -0,0 +1,18 @@
+''' import math
+print(math.int(3.5))
+
+a=5
+b=3.6
+c="Hi,This is Riyad"
+print(type(a))
+
+str1 = "this is my first python programe"
+print(str1.capitalize())
+print(str1.find("this"))
+print(str1.upper())
+print(str1.lower())'''
+
+items = ["Hi",2,'H']
+items[0] = "Hello"
+print(items)
+print(type(items))
