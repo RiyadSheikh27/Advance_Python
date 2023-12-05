8c3ec9baf1eb79ff1b4dcc5983508b0f60d1bf89
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-12-06 03:44:40 +0600

    New Code Added

---
+++ Arithmetic.py
@@ -0,0 +1,17 @@
+number1 = 30
+number2 = 20
+
+Addition = number1 + number2
+Substraction = number1 - number2
+mul = number1 * number2
+div = number1 / number2
+
+print(f"Substration = {Substraction}")
+print(f"Multiplication = {mul}")
+print(f"Addition = {Addition}")
+print(f"Div = {div}")
+
+name = "Fazle"
+name1 = "Rabbi"
+name2 = 9
+print(f"My name is {name + " " + name1 + str(name2)}")
